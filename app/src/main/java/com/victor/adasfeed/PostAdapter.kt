4b07455b97e1fd63381a078ba6cb179d49692b09
Victor Magosso <victor_magosso@hotmail.com>
2023-02-27 22:05:03 -0300

    adding recycler view to stories and post

---
+++ app/src/main/java/com/victor/adasfeed/PostAdapter.kt
@@ -0,0 +1,155 @@
+package com.victor.adasfeed
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+data class Post(
+    val userName: String,
+    val description: String,
+    val imagePost: Int,
+    val imageUser: Int,
+    val isLiked: Boolean = false,
+    val isFavorite: Boolean = false,
+)
+
+private fun mockedPostList() = mutableListOf(
+    Post(
+        userName = "Jonas do Dark",
+        description = "Hoje foi dia de muita gravação !!!",
+        imagePost = R.drawable.feed1,
+        imageUser = R.drawable.user1,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ), Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+    Post(
+        userName = "Marcela",
+        description = "Olhem meus doguinhos que fofos",
+        imagePost = R.drawable.feed2,
+        imageUser = R.drawable.user2,
+    ),
+
+)
+
+class PostAdapter(private val postList: MutableList<Post> = mockedPostList()) :
+    RecyclerView.Adapter<PostViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val postView =
+            LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false)
+        return PostViewHolder(postView)
+    }
+
+    override fun getItemCount() = postList.size
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.textUserName.text = postList[position].userName
+        holder.textPostDescription.text = postList[position].description
+        holder.imagePost.setImageResource(postList[position].imagePost)
+        holder.imageUser.setImageResource(postList[position].imageUser)
+
+        holder.imageLiked.setOnClickListener {
+            holder.imageLiked.setImageResource(R.drawable.liked_fill)
+        }
+    }
+
+    fun addNewPost(newPost: Post) {
+        postList.add(newPost)
+        notifyItemInserted(postList.indexOf(newPost))
+        // perde performance pois recria a lista inteira
+        // o DiffUtilCallback é melhor
+    }
+
+    fun setNewList(newList: List<Post>) {
+        val diffCallback = PostDiffUtilCallback(oldPostList = postList, newPostList = newList)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        postList.clear()
+        postList.addAll(newList)
+//        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
+    }
+}
+
+class PostDiffUtilCallback(
+    private val oldPostList: List<Post>,
+    private val newPostList: List<Post>,
+) : DiffUtil.Callback() {
+    override fun getOldListSize() = oldPostList.size
+
+    override fun getNewListSize() = newPostList.size
+
+    // osItensSãoOsMesmos?
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldPostItem: Post = oldPostList[oldItemPosition]
+        val newPostItem: Post = newPostList[newItemPosition]
+        return oldPostItem.javaClass == newPostItem.javaClass
+    }
+
+    // osConteudosSãoOsMesmos?
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldPostItem: Post = oldPostList[oldItemPosition]
+        val newPostItem: Post = newPostList[newItemPosition]
+        return oldPostItem.hashCode() == newPostItem.hashCode()
+    }
+}
+
+class PostViewHolder(view: View) : ViewHolder(view) {
+    val textUserName: TextView
+    val textPostDescription: TextView
+    val imageUser: ImageView
+    val imagePost: ImageView
+    val imageLiked: ImageView
+    val imageFavorite: ImageView
+
+    init {
+        textUserName = view.findViewById(R.id.textUserName)
+        textPostDescription = view.findViewById(R.id.textPostDescription)
+        imageUser = view.findViewById(R.id.imageUser)
+        imagePost = view.findViewById(R.id.imagePost)
+        imageLiked = view.findViewById(R.id.imageLiked)
+        imageFavorite = view.findViewById(R.id.imageFavorite)
+    }
+}
---
+++ app/src/main/java/com/victor/adasfeed/StoriesAdapter.kt
@@ -0,0 +1,69 @@
+package com.victor.adasfeed
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class StoriesAdapter(
+    private val storiesList: List<Stories> = mockList()
+) : RecyclerView.Adapter<StoriesAdapter.StoriesViewHolder>() {
+
+    inner class StoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textUserName: TextView
+        val textTimeInMinutes: TextView
+        val imageUser: ImageView
+        val imagePost: ImageView
+
+        init {
+            textUserName = view.findViewById(R.id.textUserName)
+            textTimeInMinutes = view.findViewById(R.id.textTimeInMinutes)
+            imageUser = view.findViewById(R.id.imageUser)
+            imagePost = view.findViewById(R.id.imagePost)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesViewHolder {
+        val storiesItemView = LayoutInflater.from(parent.context).inflate(R.layout.stories_item, parent, false)
+        return StoriesViewHolder(view = storiesItemView)
+    }
+
+    override fun getItemCount(): Int {
+        return storiesList.size
+    }
+
+    //Bind = vincular
+    //View holder = "segura" um espaço pra view
+    override fun onBindViewHolder(holder: StoriesViewHolder, position: Int) {
+        holder.textUserName.text = storiesList[position].textUserName
+        holder.textTimeInMinutes.text = storiesList[position].textTimeInMinutes
+        holder.imageUser.setImageResource(storiesList[position].imageUser)
+        holder.imagePost.setImageResource(storiesList[position].imagePost)
+
+        holder.textUserName.setOnClickListener {
+            println(holder.textUserName.text)
+        }
+    }
+}
+
+data class Stories(
+    val textUserName: String,
+    val textTimeInMinutes: String,
+    val imageUser: Int,
+    val imagePost: Int,
+)
+
+private fun mockList() = listOf(Stories(
+    "Joao",
+    "15 m",
+    imageUser = R.drawable.universe_image,
+    imagePost = R.drawable.universe_image,
+),
+    Stories(
+        "Victor",
+        "28 m",
+        imageUser = R.drawable.paisagem_post,
+        imagePost = R.drawable.paisagem_post,
+    ))
