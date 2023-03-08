1ce7a561fe95950f35bf7577babbd10a8e1a6b61
joaovictorsl <joaovictordev2003@gmail.com>
2023-03-07 23:58:46 -0300

    ProfileActivity is now able to change the username and nickname while editing them. Changes are not yet applied to other activities.

--- app/src/main/java/com/victor/adasfeed/ProfileActivity.kt
+++ app/src/main/java/com/victor/adasfeed/ProfileActivity.kt
@@ -5,71 +5,96 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.victor.adasfeed.passandodados.User
 
 class ProfileActivity : AppCompatActivity() {
-    // quebrar o codigo de proposito
-    // private val context = applicationContext.toString()
-
     private lateinit var textUserName: TextView
     private lateinit var textNickname: TextView
     private lateinit var buttonCall: Button
     private lateinit var imageUser: ImageView
+    private lateinit var backToFeedBtn: AppCompatButton
+    private lateinit var etName: EditText
+    private lateinit var etNickname: EditText
+    private lateinit var fabSave: FloatingActionButton
+
+    private lateinit var dialUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        Log.d("contexto PA", applicationContext.toString())
-        Log.d("ciclo de vida PA", "onCreate")
+        initViews()
+        handleIntentExtras()
+        handleSetClickListeners()
+        handleAddTextChangedListener(etName, textUserName, R.string.profile_name)
+        handleAddTextChangedListener(etNickname, textNickname, R.string.profile_nickname)
+    }
 
+    private fun initViews() {
         textUserName = findViewById(R.id.textUserName)
         textNickname = findViewById(R.id.textNickname)
         buttonCall = findViewById(R.id.buttonContact)
         imageUser = findViewById(R.id.imageUser)
+        backToFeedBtn = findViewById(R.id.buttonReturnToFeed)
+        etName = findViewById(R.id.editUserName)
+        etNickname = findViewById(R.id.editNickaname)
+        fabSave = findViewById(R.id.fabSave)
+    }
 
+    private fun handleSetClickListeners() {
+        buttonCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, dialUri)
+            startActivity(intent)
+        }
+
+        backToFeedBtn.setOnClickListener { finish() }
+
+        fabSave.setOnClickListener {
+
+        }
+    }
+
+    private fun handleIntentExtras() {
         val extras = intent.extras
-        var uri = Uri.parse("")
+        dialUri = Uri.parse("")
 
-        // envia por intents - putExtra
-        // recebe por Bundle
         extras?.let { bundle ->
             val user = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 bundle.getParcelable(EXTRA_KEY, User::class.java)
             } else {
                 bundle.getParcelable(EXTRA_KEY) as? User
             }
-            // mais de um parametro na string
-//            textUserName.text = getString(R.string.profile_name, safeUser.userName, safeUser.userNickname)
 
             user?.let { safeUser ->
                 textUserName.text = getString(R.string.profile_name, safeUser.userName)
                 textNickname.text = safeUser.userNickname
                 imageUser.setImageResource(safeUser.imageUser)
-                uri = safeUser.tel?.let {
+                dialUri = safeUser.tel?.let {
                     Uri.parse("tel:${safeUser.tel}")
                 } ?: Uri.parse("tel:")
             }
         }
-
-        buttonCall.setOnClickListener {
-            val intent = Intent(Intent.ACTION_DIAL, uri)
-            startActivity(intent)
-            finish()
-        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("ciclo de vida PA", "onStart")
-    }
+    private fun handleAddTextChangedListener(editText: EditText, target: TextView, stringRes: Int) {
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("ciclo de vida PA", "onResume")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                target.text = getString(stringRes, s.toString())
+            }
+
+        })
     }
 }
